df5f1841483826f30e1c97ad7fa030ce0e793445
EricTGoto <erictomokigoto@gmail.com>
2022-07-21 23:12:14 -0700

    began integrating mongodb, installed mongoose, dotenv

---
+++ models/mongo.js
@@ -0,0 +1,11 @@
+require('dotenv').config({ path: '../.env' });
+const mongoose = require('mongoose');
+
+const url = process.env.MONGODB_URI;
+mongoose.connect(url)
+  .then((result) => {
+    console.log('conntected');
+  })
+  .catch((result) => {
+    console.log(result);
+  });
